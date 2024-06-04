6e4c4f35c89e3598869fb41744f8d4d1cd06d008
Bobur98 <bmuhibullaev@mail.ru>
2024-06-05 02:43:43 +0900

    feat: meta tags aintegrations on documetn private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta charSet="UTF-8" />
+      <meta name="title" content="Nestar demo" />
+      <meta name="robots" content="index,follow" />
+      <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+      {/**SEO **/}
+      <meta
+        name="keyword"
+        content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+      />
+      <meta
+        name="description"
+        content={
+          "Buy and sell properties anyware anytime in South Korea. | " +
+          "Купить и продать недвижимость в любое время в Южной Корее. | " +
+          "대한민국 어디에서나 언제든지 부동산을 사고 팔 수 있습니다."
+        }
+      />
       <body>
         <Main />
         <NextScript />
