6fa3ea2a4ed3b0ab41789342df14bbc5eb5447b7
Bobur98 <bmuhibullaev@mail.ru>
2024-06-14 00:39:54 +0900

    feat: apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,30 @@ import PopularProperties from "@/libs/components/homePage/PopularProperties";
 import TopAgents from "@/libs/components/homePage/TopAgents";
 import TopProperties from "@/libs/components/homePage/TopProperties";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertieRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 10,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPropertiesData =>", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>Mobile Hompage</Stack>;
